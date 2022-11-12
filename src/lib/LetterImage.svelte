55ffdb0faebfd846dcba24e19e25ee236d0f7209
Josh White <jojawhi@gmail.com>
2022-11-11 16:05:20 -0800

    comment cleanup and minor layout adjustment for letter I

--- src/lib/LetterImage.svelte
+++ src/lib/LetterImage.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
+
 	export let src: string;
 	export let alt: string;
 	export let id: number;
 </script>
 
-<div class="container justify-center align-center">
-	<img class={id === 7 ? 'max-h-40 px-8' : 'max-h-52'} {src} {alt} />
+<div class="container justify-center align-center" transition:fade>
+	<img class={id === 7 ? 'max-h-40 px-4' : 'max-h-52'} {src} {alt} />
 </div>
--- src/lib/Name.svelte
+++ src/lib/Name.svelte
@@ -6,16 +6,9 @@
 <div class="container grid grid-cols-7 gap-6 mx-auto text-center py-20">
 	{#each $nameStore as letter}
 		{#if letter.active}
-			<!-- <span class="active text-white text-7xl font-bold p-4">{letter.image}</span> -->
 			<LetterImage id={letter.id} src={letter.image} alt={letter.alt} />
 		{:else}
 			<span class="text-white text-7xl font-bold p-4">{letter.letter}</span>
 		{/if}
 	{/each}
 </div>
-
-<style>
-	/* .active {
-		font-family: "'89 Speed Affair Gradient";
-	} */
-</style>
