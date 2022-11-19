f0585c927baaeff9abe02c44b0b6a3bb6d451718
Josh White <jojawhi@gmail.com>
2022-11-19 01:09:56 -0800

    added kodama assets and created kodama component; added animation and letter activation function to kodama; still need to add fade-out effect; fixed mobile sizing for social icons

---
+++ src/lib/Kodama.svelte
@@ -0,0 +1,79 @@
+<div class="container flex flex-col justify-center items-center">
+	<img
+		class="kodama-head rattle h-14"
+		src="/assets/images/nameLetters/kodama-head.svg"
+		alt="kodama-head"
+	/>
+	<img
+		class="kodama-body h-16 -mt-[5px]"
+		src="/assets/images/nameLetters/kodama-body.svg"
+		alt="kodama-body"
+	/>
+</div>
+
+<style>
+	/* .kodama-head {
+		height: 3rem;
+	}
+
+	.kodama-body {
+		height: 4rem;
+	} */
+
+	.rattle {
+		animation: rattle 5000ms ease-in-out infinite;
+	}
+
+	@keyframes rattle {
+		0% {
+			transform: rotate(0deg);
+		}
+
+		20% {
+			transform: rotate(55deg);
+		}
+
+		32.5% {
+			transform: rotate(55deg);
+		}
+
+		35% {
+			transform: rotate(60deg);
+		}
+
+		36.5% {
+			transform: rotate(50deg);
+		}
+
+		38% {
+			transform: rotate(-50deg);
+		}
+
+		39.5% {
+			transform: rotate(50deg);
+		}
+
+		41% {
+			transform: rotate(-50deg);
+		}
+		42.5% {
+			transform: rotate(50deg);
+		}
+
+		43% {
+			transform: rotate(-50deg);
+		}
+
+		44.5% {
+			transform: rotate(50deg);
+		}
+
+		46% {
+			transform: rotate(0deg);
+		}
+
+		100% {
+			transform: rotate(0deg);
+		}
+	}
+</style>
--- src/lib/SocialIcons.svelte
+++ src/lib/SocialIcons.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Github from '$lib/Github.svelte';
 	import Linkedin from '$lib/Linkedin.svelte';
+	import Button from './common/Button.svelte';
+	import Kodama from './Kodama.svelte';
 	import { nameStore } from './stores/name';
 	import { openAndCloseAfterTimeout } from './stores/drawer';
 
@@ -17,10 +19,13 @@
 	// 	}
 	// ];
 
+	let fadeOut = false;
+
 	const activateLetter = (index: number) => {
 		if (!$nameStore[index].active) {
 			$nameStore[index].active = true;
 			openAndCloseAfterTimeout();
+			fadeOut = true;
 		} else {
 			return;
 		}
@@ -32,7 +37,7 @@
 		href="https://github.com/jojawhi"
 		target="_blank"
 		rel="noopener"
-		class="github w-16 transition-all duration-300"
+		class="github w-12 sm:w-16 transition-all duration-300"
 	>
 		<Github />
 	</a>
@@ -40,14 +45,14 @@
 		href="https://www.linkedin.com/in/jojawhi/"
 		target="_blank"
 		rel="noopener"
-		class="linkedin w-16 transition-all duration-300"
+		class="linkedin w-12 sm:w-16  transition-all duration-300"
 	>
 		<Linkedin />
 	</a>
-	<!-- Kodama goes here when ready -->
-	<!-- <button on:click={() => activateLetter(0)}>
-		<div class="rattle w-8 h-10 bg-white rounded-2xl" />
-	</button> -->
+
+	<Button buttonType="clear" onClick={() => activateLetter(0)}>
+		<Kodama />
+	</Button>
 </div>
 
 <style>
@@ -58,7 +63,7 @@
 
 	.github:hover {
 		fill: #14b8a6; /*url(#GithubGradient2)*/
-		animation: spin 1000ms ease forwards;
+		animation: spin 500ms ease forwards;
 	}
 
 	.linkedin {
@@ -69,11 +74,7 @@
 	.linkedin:hover {
 		fill: #14b8a6; /*url(#LinkedinGradient2)*/
 		/* transform: scale(1.1); */
-		animation: spin 1000ms ease-in-out forwards;
-	}
-
-	.rattle:hover {
-		animation: rattle 1000ms ease-in-out forwards;
+		animation: spin 500ms ease-in-out forwards;
 	}
 
 	@keyframes spin {
@@ -83,33 +84,6 @@
 
 		to {
 			transform: rotate(360deg) scale(1.1);
-		}
-	}
-
-	@keyframes rattle {
-		0% {
-			transform: rotate(0deg);
-		}
-		40% {
-			transform: rotate(50deg);
-		}
-		50% {
-			transform: rotate(50deg);
-		}
-		70% {
-			transform: rotate(-50deg);
-		}
-		77.5% {
-			transform: rotate(40deg);
-		}
-		85% {
-			transform: rotate(-40deg);
-		}
-		/* 92.5% {
-			transform: rotate(20deg);
-		} */
-		100% {
-			transform: rotate(0deg);
 		}
 	}
 </style>
