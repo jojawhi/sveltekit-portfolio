10958e2b6a059810ae5383af956b7a2c2bdb6a65
Josh White <jojawhi@gmail.com>
2024-01-15 23:42:02 -0800

    tag alignment fix

--- src/lib/Tag.svelte
+++ src/lib/Tag.svelte
@@ -5,15 +5,21 @@
 <div
 	class="tag text-sm md:text-base rounded-full px-4 md:px-6 py-2 w-min transition-all duration-300 bg-gradient-to-r to-fuchsia-600 from-indigo-600"
 >
-	<p class="font-bold text-white whitespace-nowrap">{tag}</p>
+	<span class="font-bold text-white whitespace-nowrap">{tag}</span>
 </div>
 
 <style>
 	.tag {
-		display: inline-block;
 		user-select: none;
 	}
-	.tag:hover {
+	/* .tag:hover {
 		transform: scale(1.025);
+	} */
+
+	.tag > span {
+		display: flex;
+		justify-content: baseline;
+		align-items: baseline;
+		margin-top: 0.5rem;
 	}
 </style>
